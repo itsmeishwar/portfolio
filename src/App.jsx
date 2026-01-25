f4203f0478562843c900080ae01ad4e4e13b23b9
itsmeishwar <ishwarawasthi3@gmail.com>
2026-01-25 21:23:22 +0545

    make some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
-import { BrowserRouter } from 'react-router-dom';
-import Navbar from './components/ui/Navbar';
-import StarsCanvas from './components/canvas/Stars';
-import Hero from './components/sections/Hero';
-import About from './components/sections/About';
-import Experience from './components/sections/Experience';
-import Works from './components/sections/Works';
-import BlogSection from './components/sections/BlogSection'; // New Blog section
-import Contact from './components/sections/Contact';
-import Footer from './components/ui/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import BlogPage from './pages/BlogPage';
+import BlogPostPage from './pages/BlogPostPage';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-primary bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>
-
-        <About />
-        <Experience />
-        <Works />
-        <BlogSection /> {/* Add Blog section */}
-        <Contact />
-        <StarsCanvas />
-
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/blog/:id" element={<BlogPostPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
