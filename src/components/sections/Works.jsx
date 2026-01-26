78e29386eed176eacda08063202bbe93f90dfb65
itsmeishwar <ishwarawasthi3@gmail.com>
2026-01-26 22:44:11 +0545

    change the works section

--- src/components/sections/Works.jsx
+++ src/components/sections/Works.jsx
@@ -34,7 +34,7 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
                 </div>
 
                 <div className='mt-5'>
-                    <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+                    <h3 className='text-gray-900 dark:text-white font-bold text-[24px]'>{name}</h3>
                     <p className='mt-2 text-secondary text-[14px]'>{description}</p>
                 </div>
 
