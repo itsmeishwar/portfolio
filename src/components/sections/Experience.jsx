587545cb3b1d1d3dede4fb5a15f58ef72deb9dbb
itsmeishwar <ishwarawasthi3@gmail.com>
2026-02-03 23:07:12 +0545

    change the about page

--- src/components/sections/Experience.jsx
+++ src/components/sections/Experience.jsx
@@ -52,19 +52,6 @@ const experiences = [
             "Participating in code reviews and providing constructive feedback to other developers.",
         ],
     },
-    {
-        title: "Full Stack Developer",
-        company_name: "Meta",
-        icon: <FaBriefcase />,
-        iconBg: "#E6DEDD",
-        date: "Jan 2023 - Present",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
 ];
 
 const ExperienceCard = ({ experience }) => (
