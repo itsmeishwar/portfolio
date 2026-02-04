64564ac1c8c7418edd6f60516f14d81e87a8533d
itsmeishwar <ishwarawasthi3@gmail.com>
2026-02-04 21:11:04 +0545

    change the gmail

--- src/components/sections/Experience.jsx
+++ src/components/sections/Experience.jsx
@@ -35,7 +35,7 @@ const experiences = [
         date: "Jan 2025",
         points: [
             "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Collaborating with cross-functional teams including designers,  and other developers to create high-quality products.",
             "Implementing responsive design and ensuring cross-browser compatibility.",
             "Participating in code reviews and providing constructive feedback to other developers.",
         ],
