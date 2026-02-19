3a82b39960f4a0ddf7a89734b1e1af44253fbfb8
itsmeishwar <ishwarawasthi3@gmail.com>
2026-02-19 23:34:00 +0545

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import BlogPage from './pages/BlogPage';
 import BlogPostPage from './pages/BlogPostPage';
-
+// this os the main app component that sets up the routing for the application. It uses React Router to define the routes for the landing page, blog page, and individual blog post pages. Each route is associated with a specific component that will be rendered when the route is accessed.
 const App = () => {
   return (
     <BrowserRouter>
