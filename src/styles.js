73c6f3eb839e3c6c22fb24533ce791f9c912f1f6
itsmeishwar <ishwarawasthi3@gmail.com>
2026-09-02 14:12:42 +0545

    update text color for hero and section headers to improve readability

--- src/styles.js
+++ src/styles.js
@@ -4,12 +4,12 @@ const styles = {
     padding: "sm:px-16 px-6 sm:py-16 py-10",
 // this is a JavaScript object that defines various styles used throughout the application. It includes padding styles for different screen sizes, as well as specific styles for hero text and section headers. The styles are defined using Tailwind CSS utility classes, which allow for responsive design and easy customization. The object is exported for use in other components of the application.
     heroHeadText:
-        "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+        "font-black text-gray-900 dark:text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
     heroSubText:
         "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
 
     sectionHeadText:
-        "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+        "text-gray-900 dark:text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
     sectionSubText:
         "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
 };
